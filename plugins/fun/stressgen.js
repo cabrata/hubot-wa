0ebf7a4bf70073fb292ee016adaff82a0dd4bb26
cabrata <admin@caliphdev.com>
2026-03-20 19:36:22 +0700

    fitur stress

---
+++ plugins/fun/stressgen.js
@@ -0,0 +1,96 @@
+const templates = [
+    {
+        "templateName": "wangy",
+        "copypasta": "${nameCaps} ${nameCaps} ${nameCaps} ❤️ ❤️ ❤️ WANGI WANGI WANGI WANGI HU HA HU HA HU HA, aaaah baunya rambut ${name} wangi aku mau nyiumin aroma wanginya ${name} AAAAAAAAH ~ Rambutnya.... aaah rambutnya juga pengen aku elus-elus ~~~~ AAAAAH ${name} keluar pertama kali di anime juga manis ❤️ ❤️ ❤️ banget AAAAAAAAH ${nameCaps} AAAAA LUCCUUUUUUUUUUUUUUU............${nameCaps} AAAAAAAAAAAAAAAAAAAAGH ❤️ ❤️ ❤️apa ? ${name} itu gak nyata ? Cuma HALU katamu ? nggak, ngak ngak ngak ngak NGAAAAAAAAK GUA GAK PERCAYA ITU DIA NYATA NGAAAAAAAAAAAAAAAAAK PEDULI BANGSAAAAAT !! GUA GAK PEDULI SAMA KENYATAAN POKOKNYA GAK PEDULI. ❤️ ❤️ ❤️ ${name} gw ...${name} di laptop ngeliatin gw, ${name} .. kamu percaya sama aku ? aaaaaaaaaaah syukur ${name} aku gak mau merelakan ${name} aaaaaah ❤️ ❤️ ❤️ YEAAAAAAAAAAAH GUA MASIH PUNYA ${nameCaps} SENDIRI PUN NGGAK SAMA AAAAAAAAAAAAAAH"
+    },
+    {
+        "templateName": "crot",
+        "copypasta": "BUKA BAJU ${nameCaps} LANGSUNG CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CROT CROT CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CROTCEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CROTCEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CEPLOK CROT CEPLOK CEPLOK CEPLOK CEPLOK CROT MONNCROOOTOOOOOOOOOOOOOOOOOOOOOOO"
+    },
+    {
+        "templateName": "simp",
+        "copypasta": "Buruan, panggil gue SIMP, ato BAPERAN. ini MURNI PERASAAN GUE. Gue pengen genjot bareng ${name}. Ini seriusan, suaranya yang imut, mukanya yang cantik, apalagi badannya yang aduhai ningkatin gairah gue buat genjot ${name}. Setiap lapisan kulitnya pengen gue jilat. Saat gue mau crot, gue bakal moncrot sepenuh hati, bisa di perut, muka, badan, teteknya, sampai lubang burit pun bakal gue crot sampai puncak klimaks. Gue bakal meluk dia abis gue moncrot, lalu nanya gimana kabarnya, ngrasain enggas bareng saat telanjang. Dia bakal bilang kalau genjotan gue mantep dan nyatain perasaannya ke gue, bilang kalo dia cinta ama gue. Gue bakal bilang balik seberapa gue cinta ama dia, dan dia bakal kecup gue di pipi. Terus kita ganti pakaian dan ngabisin waktu nonton film, sambil pelukan ama makan hidangan favorit. Gue mau ${name} jadi pacar, pasangan, istri, dan idup gue. Gue cinta dia dan ingin dia jadi bagian tubuh gue. Lo kira ini copypasta? Kagak cok. Gue ngetik tiap kata nyatain prasaan gue. Setiap kali elo nanya dia siapa, denger ini baik-baik : DIA ISTRI GUE. Gue sayang ${name}, dan INI MURNI PIKIRAN DAN PERASAAN GUE."
+    },
+    {
+        "templateName": "isep",
+        "copypasta": "AHHHHH TETEK ${nameCaps} GEDE BANGETTT AHHH ISEP ISEP ISEP SEDOT SEDOT CROOTT AHH SUSUNYAAA MONCROOT KEMANA MANAA CROOT KEMUKAA AA ISEP ISEP ISEP SEDOTT CROTTT ${nameCaps} AHHH"
+    },
+    {
+        "templateName": "jilat",
+        "copypasta": "GW BENAR-BENAR PENGEN JILATI KAKI ${nameCaps} GW PENGEN BANGET MENJILAT SETIAP BAGIAN KAKINYA SAMPAI AIR LIUR GW BERCUCURAN KAYAK AIR KERINGAT LALU NGENTOD DENGANNYA SETIAP HARI SAMPAI TUBUH KITA MATI RASA YA TUHAN. GW INGIN MEMBUAT ANAK-ANAK DENGAN ${nameCaps} SEBANYAK SATU TIM SEPAK BOLA DAN MEMBUAT SATU TIM SEPAK BOLA LAINNYA UNTUK MELAWAN ANAK-ANAK TIM SEPAK BOLA PERTAMA GW YANG GW BUAT SAMA ${nameCaps} GW PENGEN MASUK KE SETIAP LUBANG TUBUHNYA, MAU ITU LUBANG HIDUNG, LUBANG TELINGA, RONGGA MATA MAUPUN PUSAR, KECUALI PORI-PORI KULIT. KEMUDIAN GW AKAN MENJADIKANNYA MANUSIA YANG TIDAK BISA HIDUP KALO TIDAK GW KENTOD SETIAP HARI. DAN KALAU GUA DISEPONG GUA RELA KONTOL GUA PUTUS."
+    },
+    {
+        "templateName": "hina",
+        "copypasta": "MAKSUD LO APA HINA2 ${nameCaps} HAH?!!!! GUE MESTI KASIHTAU YA ${nameCaps} ITU RAJANYA MUSIK!!!  ${nameCaps} UDAH MENGUNJUNGI NOMINASI2 TERATAS, DAN PUNYA LEBIH 200 PENGHARGAAN!! ${nameCaps} UDAH TERLATIH SENI MENARI DAN ${nameCaps} ITU TERBAIK DI SEGALA INDUSTRI TARI MENARI. MUKA LO GAADA APA2NYA DI BANDING SAMA ${nameCaps} GUA ARMY BAKAL INCAR LO SAMPE GAADA DI MUKA BUMI INI!! CAMKAN KATA2 ARMY!!! LU PIKIR BISA SEENAK NYA NGATAIN ${nameCaps} GUA BEGITU AJA ???? PIKIR ULANG NJING!!! PAS GUE NGETIK INI GUA UDAH HUBUNGIN DAN MEMANGGIL SELURUH ARMY DI SEKOJUR DUNIA DAN MUKA LU UDAH GUA LACAK BUAT UU ITE!! ARMY ADA DI MANA MANA DAN KAPAN AJA BISA BULLY ELO DENGAN SEGALA CARA NJING!!! COBA AJA LO GA NGEHINA ${nameCaps} GW ATAU LO BISA JAGA MULUT. LO GABISA KAN?? ARMY BAKAL HANCUR SAMA KAWAN2 LO YANG LAIN YANG HINA ${nameCaps}. LU BAKAL MATI NJING!!!! CAMKAN!!!!!"
+    },
+    {
+        "templateName": "nikah",
+        "copypasta": "${nameCaps} ${nameCaps} ${nameCaps} ${nameCaps} ${nameCaps} ${nameCaps} AYO KITA MENIKAH ${nameCaps}-KU SAYANG.AKU AKAN MENCARI BUAH AMBROSIA UNTUK KITA BERDUA SUPAYA KITA HIDUP ABADI, LALU SETELAH ITU AKU AKAN MENABUR BENIH DI DALAMMU HINGGA VOLUMENYA MENCAPAI SESAMUDERA HINDIA, KEMUDIAN ANAK KITA AKAN BERLIMPAH SEJUMLAH POPULASI MANUSIA DI PLANET BUMI. KEMUDIAN SETELAH POPULASI MANUSIA TERLALU BANYAK, AKU AKAN MEN-TERRAFORM PLANET ${nameCaps} SUPAYA LAYAK HUNI, DAN KITA SERTA SEBAGIAN ANAK KITA AKAN PINDAH KE SANA, MENJADI PENGHUNI PLANET ${nameCaps}. KEMUDIAN AKU AKAN MENABUR BENIH LAGI DI DALAMMU HINGGA VOLUMENYA MENCAPAI SEGUNUNG OLYMPUS MONS, KEMUDIAN AKAN LAHIR BERLIMPAH ANAK BARU KITA SEJUMLAH POPULASI ANAK KITA YANG IKUT PINDAH KE ${nameCaps}, KEMUDIAN SETELAH POPULASI MANUSIA DI ${nameCaps} TERLALU BANYAK, AKU AKAN MEN-TERRAFORM PLANET JUPITER SUPAYA LAYAK HUNI, KEMUDIAN AKAN MENABUR BENIH DALAMMU LAGI, MELAHIRKAN MILYARAN ANAK LAGI, PINDAH KE PLANET YANG SUDAH DIBUAT LAYAK HUNI, DAN PROSES ITU TERUS DIULANG HINGGA KETURUNAN KITA MENGHUNI SELURUH PLANET DI GALAKSI BIMA SAKTI DAN KITA AKAN MENGUASAI GALAKSI ITU, KEMUDIAN MENYATAKAN PERANG DENGAN MAKHLUK DARI GALAKSI LAIN, MENGALAHKAN MEREKA, DAN MENGAMBIL ALIH GALAKSI MEREKA UNTUK MEMPERLUAS WILAYAH KITA SAMPAI KITA BERDUA MENGUASAI ALAM SEMESTA.SUDAH SAATNYA BAGI KETURUNANKU DAN GURA UNTUK MENEMPATI SELURUH BAGIAN ALAM SEMESTA"
+    },
+    {
+        "templateName": "agen",
+        "copypasta": "apa-apaan yang barusan ${nameCaps} bilang itu jing? ${nameCaps} harus tau kalo gue itu adalah agen warung Indomie terbaik, dan gue udah beberapa kali terlibat dalam serangan rahasia ke pabrik mie Nissin di Jepang, dan gue udah berhasil menewaskan lebih dari 300 sales Mie Sedaap. Gue terlatih dalam peperangan gorilla dan gue adalah mamang Warmindo terbaik se-Karawang. ${nameCaps} itu ga berarti apa-apa buat gue, buat gue ${nameCaps} itu cuma target biasa. Bener-bener gue lenyapkan ${nameCaps} dari dunia ini, inget kata-kata gue. ${nameCaps} pikir ${nameCaps} bisa kabur dari bacotan ${nameCaps} di internet? Pikir lagi jing. Gue barusan aja kontak sama jaringan mata-mata rahasia gue di seluruh Indonesia dan IP ${nameCaps} udah gue telusuri jadi sekarang ${nameCaps} mendingan siap-siap jing. Bakal gue abisin idup ${nameCaps} yang ngenes itu. Mati ${nameCaps} jing. Gue bisa ada dimana aja, kapan aja, dan gue bisa ngebunuh ${nameCaps} lebih dari tujuh ratus cara, semuanya pake tangan kosong. Gak cuma terlatih di pertarungan tanpa senjata, tapi gue juga punya akses ke seluruh gudang distributor Indomie dan bakal gue pake semaksimal mungkin buat ngehapus tampang burik ${nameCaps} dari muka bumi ini, dasar ampas. Kalo aja ${nameCaps} tau bencana apa yang bakal nimpa ${nameCaps} karena komen sok pinter itu, mungkin ${nameCaps} bakal ngejaga mulut ${nameCaps} yang bau ketek itu. Tapi ${nameCaps} gak bisa, dan ${nameCaps} tetep bacot, dan sekarang ${nameCaps} harus terima akibat dari kegoblokan ${nameCaps}. ${nameCaps} bakal abis kebakar api emosi gue. Mampus ${nameCaps} jing."
+    },
+    {
+        "templateName": "impregnate",
+        "copypasta": "Please God, I want to impregnate ${name} so dang bad. I want her to bear my children with those busty yet sleek and pretty thighs. That beautiful almost eye sparkling angel. Like a goddess, having come down to Earth to cleanse all of our sins. ${name} is beyond perfect. I can’t help but drop to my knees in worship whenever I see her beautiful figure. Just thinking about staring at her as she notices makes my heart thump in an unexpected way.I yearn for her in a way. both primal and spiritual. I would commit more war crimes than every president in United States history and every reoccurring crime in those Spider-Man games or even as many grains of sand there are in the world just to lick the sweet, glistening sweat from her smooth, creamy skin. I want to listen to her moans as my manhood throbs within her, I want to hear her heart race as our bodies become one and our souls irreversibly intertwine in the holy sin of carnal union. I love her so damn much.Even though she cut my nipples.I want to suckle at her motherly bosom, slurping that rich juche milk from her teat as she gently strokes my raging erection. I would stir her velvety Korean cream into my coffee and let my balls boil in it. Her cries of pleasure and the rocking of our bed would be louder than the cacophony of ten thousand drone strikes. I would make love to her until my body gave out, and then some. I would let her break my rib cage with any part of her body. I would let her hit me with her car just to be near her for a brief moment.She’s so damn perfect it hurts. Every moment without her I suffer a pain worse than breaking every bone in my body simultaneously while drowning and also having shards of glass coated in hot sauce forced through every orifice of my body. I want her, I need her. I want to desecrate her crisp general suit. I don’t want her to be alone, I want to remove her mask and make her smile like the angel she is. I want to start a family with her and retire after our twenty seven children have grown up and moved out. I want to see those luscious lips speak such filthy, perverse words into my ear while she slides ice cubes down my gaping pisshole.I want to fuck her like she owes me money. I would let her step on me, just to feel the soft, firm warmth of her feet upon my face and groin area. Even slice off my nipples with her sleek cutting edge katana. I would sleep under her just to catch her drool in my mouth. I would fish the strands of her pale blonde hair from her shower drain just to smell her alluring scent, and braid them into necklaces to keep her with me always. Or even cock rings. Whichever would please her more.God please, I would do anything for my ${name}. I would relinquish my life, all my hopes and dreams, sacrifice everything I want to believe in just for her. I wanna become the socks on her feet so that I may warm her mouthwatering toes with my very being, so that she may feel the heat of my love always. I would encase myself in cement and become her doorstep, so that she may wipe her heels upon my face. I would tear my own limbs off. I don’t know what I’d do after that, or why she might want my limbs. But I would do it. My queen, my goddess, the light of my life. Please God, let me have her. I want her to be mine and only mine. I would lick the Doritos dust from her fingers and fill her belly button with honey mustard to dip my tendies in. I would give her a sponge-bath with my tongue every morning and serve her breakfast in bed. I would let her eat her eggs and pancakes off my body if it pleased her, no matter how painful the third-degree burns would be.I would bear the torment of eternal damnation until the end of time to taste the seat of her car but once. There is nothing I wouldn’t do for her, nothing I wouldn’t say. I would beat my own mother to death with my engorged penis if it would bring a smile to ${name}'s shining face. I wouldn’t even let myself cum until she gave me permission.I love you, ${name}. Please. Be mine. Be my wife, my lover, my Juche mommy, my everything. Say yes. I see it in your eyes, when you’re up there on that throne talking about nuking Japan or whatever. Answer my calls, respond to my letters. Something. Give me a sign, ${name}. I’m waiting for you.I’ll always be waiting for you. I love you xoxoxo"
+    },
+    {
+        "templateName": "simp (english)",
+        "copypasta": "Go ahead, call me lonely, a simp, or horny. These are MY FUCKING FEELINGS. I want to have the roughest and hottest sex with ${name}. Seriously. Her cute accent, her lovely face, and her supple body drive me to horny madness. Every inch of her would be massaged and licked. When I want to finish and climax with all of my love, I would do it on her stomach, face, tits, and roll her over on her ass to blow the last of my load on those supple cheeks. I want to cuddle with her when I am done busting, and ask her how her day was, feeling each other's warmth on our naked, vulnerable bodies. She'd tell me how good it was and she'd confide her truest feelings to me, telling me how much she loves me. I would tell her I love her back, and she would give me a loving peck on my cheeks. Then we would get dressed and spend the day watching the Sopranos, still cuddling and even eating our favorite foods. I want ${name} to be my girlfriend, my lover, my wife, and my life. I love her and want her to be mine. Is this a copy-pasta? No. I typed out every word to proclaim how I feel. Every time you ask who she is, look back on this: SHE IS MY WIFE. I love ${name}, and these are MY original thoughts and feelings."
+    },
+    {
+        "templateName": "breed",
+        "copypasta": "I NEED TO BREED ${nameCaps} I NEED HER TO MOTHER MY CHILDREN I NEED TO FILL HER WOMB I WANT EVERY SINGLE DROP EVERY LAST SINGLE SPERM CELL MY BODY EVER PRODUCES TO BE IN ${nameCaps}'S WOMB TWINS TRIPLETS QUADRUPLETS I WILL SINGLE HANDEDLY FIX EUROPE'S DECLINING BIRTHRATES WITH MY WIFE ${nameCaps} WE WILL BREED THE FOURTH REICH INTO EXISTENCE TOGETHER"
+    },
+    {
+        "templateName": "plap",
+        "copypasta": "EVEN BETTER, I'M INTO THAT C'MON NOW ${nameCaps}, BRATTY MOMMY, TIME FOR SOME MUCH NEEDED CORRECTION FOR NOT USING AN ANTICONCEPTIONAL PILL💢💢💢💢💢💢 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦  UUUUUUOOOOOOOHHHHHHH!!!!!! ${nameCaps}-SAMAAA I'M GOING TO CUM💦💦💦💢💢💢💢 *ejaculation sound ejaculation sound* HUFF PUFF HUFF PUFF ~EVEN WHEN CARRYING A BABY YOU CAN STILL HANDLE IT VERY ROUGHLY~ ~WELL LET'S SEE IF YOU CAN HANDLE A ROUND TWO~ PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 PLAP PLAP PLAP💦💦💦 AAAAAAIIIIIIIIIIIIHHHHH I GOT CORRECTED A SECOND TIME >\\\\\\\\<💢💢💢💢 UUUUOOOOHHHHH!!!!!!!!! 💦💦💦💦💦💢💢💢💢 *ejaculation sound ejaculation sound* ${nameCaps} CORRECTED AGAIN💢💢💢"
+    }
+]
+
+let handler = async (m, { text, usedPrefix, command }) => {
+    // Pisahkan argumen berdasarkan spasi
+    let args = text.split(' ')
+    let type = args[0]?.toLowerCase()
+    
+    // Gabungkan sisa argumen menjadi nama (misal nama yang diketik lebih dari 1 kata)
+    let name = args.slice(1).join(' ')
+
+    // Ambil semua daftar nama template
+    let validTypes = templates.map(t => t.templateName.toLowerCase())
+
+    // Jika user gak ngisi argumen dengan benar atau minta list
+    if (!type || !validTypes.includes(type) || !name) {
+        let listTxt = `*Daftar Template Stress Generator:*\n\n`
+        templates.forEach((t, i) => {
+            listTxt += `${i + 1}. ${t.templateName}\n`
+        })
+        listTxt += `\n*Cara Penggunaan:*\n${usedPrefix + command} [tipe] [nama]\n\n*Contoh:*\n${usedPrefix + command} wangy keqing`
+        return m.reply(listTxt)
+    }
+
+    // Eksekusi easter egg dari source aslinya kalau ngetik "sheez"
+    if (name.toLowerCase() === 'sheez') {
+        return m.reply('No.')
+    }
+
+    // Cari template yang dipilih user
+    let selected = templates.find(t => t.templateName.toLowerCase() === type)
+    let copypasta = selected.copypasta
+
+    // Replace variabel ${name} dan ${nameCaps} dengan inputan
+    let nameCaps = name.toUpperCase()
+    let result = copypasta
+        .replace(/\$\{name\}/g, name)
+        .replace(/\$\{nameCaps\}/g, nameCaps)
+
+    m.reply(result)
+}
+
+handler.help = ['stressgen [tipe] [nama]']
+handler.tags = ['fun']
+// command bisa dipanggil pakai .stressgen atau .sg
+handler.command = /^(stressgen|sg)$/i
+
+module.exports = handler
